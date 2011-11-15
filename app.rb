3dac96a23e28ee7e8288304329a38d4b3c991065
Pavel Argentov <argentoff@gmail.com>
2011-11-15 18:11:35 +0400

    Test suite code

--- app.rb
+++ app.rb
@@ -1,10 +1,33 @@
+# coding: utf-8
+
 require "rubygems"
 require "bundler/setup"
 require "sinatra"
 require "haml"
 
-class App < Sinatra::Base
-  get "/" do
-    haml "%p no UTF"
+module Helpers
+  def utf_in_ruby
+    "Русская строка в Ruby"
   end
 end
+
+class App < Sinatra::Base
+  get "/" do
+    haml :index_no_utf
+  end
+
+  get "/utf_in_ruby" do
+    haml :index_utf_ruby
+  end
+
+  get "/utf_in_haml" do
+    haml :index_utf_haml
+  end
+
+  get "/utf_in_ruby_and_haml" do
+    haml :index_utf_ruby_haml
+  end
+
+  include Helpers
+end
+
